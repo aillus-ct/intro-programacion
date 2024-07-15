25857b39e60c3060b7cab13fd02a488d3b072eb9
Emiliano Nunez <nunez.emiliano@gmail.com>
2024-07-15 19:18:27 -0300

    Agrego los ejemplos del encuentro 5

---
+++ alumnos/EmilianoNunez/Encuentro5/ParImpar.java
@@ -0,0 +1,10 @@
+public class ParImpar {
+        public static void main(String[] args) {
+        int x = 2;
+        if (x > 0) {
+            System.out.println("x es positivo");
+        } else {
+            System.out.println("x no es positivo");
+        }
+        }
+}
