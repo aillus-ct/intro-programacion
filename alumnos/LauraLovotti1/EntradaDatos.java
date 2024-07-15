2676b809398198a890b95a14f04ed4c30b7b23e2
LauraLovo <lauralovotti2@hotmail.com>
2024-07-15 19:21:33 -0300

    mis primeros ejercicios

---
+++ alumnos/LauraLovotti1/EntradaDatos.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EntradaDatos{
+
+    public static void main(String[]args) {
+
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Ingrese un nombre: ");
+        String nombre = entrada.nextLine();
+
+        
+        System.out.println("Ingrese un apellido");
+        String apellido = entrada.nextLine();
+    
+    }
+} 
