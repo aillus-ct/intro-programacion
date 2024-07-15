4610763fd3175919594417d38c82b5b9642dad54
LauraLovo <lauralovotti2@hotmail.com>
2024-07-15 20:07:56 -0300

    cambios package

--- alumnos/LauraLovotti1/EntradaDatos.java
+++ alumnos/LauraLovotti1/EntradaDatos.java
@@ -1,3 +1,4 @@
+package LauraLovotti1;
 import java.util.Scanner;
 
 public class EntradaDatos{
--- alumnos/LauraLovotti1/Pipati.java
+++ alumnos/LauraLovotti1/Pipati.java
@@ -1,4 +1,4 @@
-package LauraLovotti;
+package LauraLovotti1;
 import java.util.Random;
 import java.util.Scanner;
 
