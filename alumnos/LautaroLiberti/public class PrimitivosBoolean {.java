1e2ebd54bab19eb0b96a1914e937aacb0f8f3f9c
Lautaro Liberti <lautaritoliberti@gmail.com>
2024-07-16 15:15:53 -0300

    hola

---
+++ alumnos/LautaroLiberti/public class PrimitivosBoolean {.java
@@ -0,0 +1,6 @@
+ public class PrimitivosBoolean {
+    public static void main(String[] args) {
+        boolean varBoolean = true;
+        System.out.println("Valor de varBoolean = " + varBoolean);
+ }
+}
---
+++ alumnos/LautaroLiberti/public class PrimitivosCaracteres {.java
@@ -0,0 +1,6 @@
+public class PrimitivosCaracteres {
+    public static void main(String[] args) {
+        System.out.println();
+        char caracter = 'a';
+        System.out.println("Caracter: " + caracter);
+}
