940e1ea6c7a5ce9352a20274fde394e881fc8ca0
Emiliano Nunez <nunez.emiliano@gmail.com>
2024-08-26 20:03:28 -0300

    Actualizacion Mascotas

---
+++ doc/13-encuentro/Entrenador.java
@@ -0,0 +1,8 @@
+public class Entrenador {
+    void entrenar(Mascota mascota){
+        mascota.comer();
+        mascota.moverse();
+        mascota.descansar();
+        mascota.moverse();
+    }
+}
--- doc/13-encuentro/UsarMascotas.java
+++ doc/13-encuentro/UsarMascotas.java
@@ -1,10 +1,40 @@
 public class UsarMascotas {
     public static void main(String[] args) {
+        
         Perro perro = new Perro("Boby",10, "caniche", "marron");
         Ave ave = new Ave("Tweety",5, "canario", "amarillo");
+        Pez pez = new Pez("Nemo", 10, "mojarrita", "anaranjado");
+
         perro.saludar();
         ave.saludar();
         perro.descansar();
         perro.corre();
+        pez.comer();
+
+        perro.mostrarEnergia();
+        ave.mostrarEnergia();
+        pez.mostrarEnergia();
+
+        Entrenador entrenador = new Entrenador();
+        entrenador.entrenar(ave);
+        entrenador.entrenar(perro);
+        entrenador.entrenar(pez);
+
+        perro.mostrarEnergia();
+        ave.mostrarEnergia();
+        pez.mostrarEnergia();
+
+        Mascota[] mascotas = new Mascota[3];
+        mascotas[0] = new Perro("Firulai", 1, "galgo", "marron");
+        mascotas[1] = new Pez("Nemo", 4, "payaso", "naranja");
+        mascotas[2] = new Ave("Tweety", 10, "canario", "amarillo");
+
+        for (Mascota mascota : mascotas) {
+            mascota.comer();
+            mascota.mostrarEnergia();
+        }
+
+
+
     }
 }
