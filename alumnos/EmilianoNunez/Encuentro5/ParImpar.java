e1c4cf98d8cdb047037ae95842308bd23f5ce7df
Emiliano Nunez <nunez.emiliano@gmail.com>
2025-07-21 19:21:12 -0300

    Cambio del print de par / impar

--- alumnos/EmilianoNunez/Encuentro5/ParImpar.java
+++ alumnos/EmilianoNunez/Encuentro5/ParImpar.java
@@ -2,9 +2,9 @@ public class ParImpar {
         public static void main(String[] args) {
         int x = 2;
         if (x > 0) {
-            System.out.println("x es positivo");
+            System.out.println("el numero x es positivo");
         } else {
-            System.out.println("x no es positivo");
+            System.out.println("el numero x no es positivo");
         }
         }
 }
